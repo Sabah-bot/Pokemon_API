9b3e4e568268375796eae02a45aef9897bfa0136
KhanZuh <khanzuhair96@live.co.uk>
2024-07-03 15:27:01 +0100

    App.jsx set-up + file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path = "/" element={<Home/>} />
+          <Route path = "/pokemon" element={<PokemonList/>} />
+          <Route path = "/pokemon/:id" element={<Pokemon/>} />
+        </Routes>
+        <Footer/>
+      </Router>
+
     </>
-  )
+  );
 }
 
 export default App
